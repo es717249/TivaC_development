0140dccd8a7dd11379603a7e7a57169b701d2ce1
es717249 <es717249@iteso.mx>
2018-12-13 00:14:48 -0600

    Adding Projects/ directory with project0

---
+++ Projects/project0/rvmdk/project0.d
@@ -0,0 +1,7 @@
+.\rvmdk\project0.o: project0.c
+.\rvmdk\project0.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\rvmdk\project0.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\rvmdk\project0.o: ..\..\..\..\inc/hw_types.h
+.\rvmdk\project0.o: ..\..\..\..\inc/hw_memmap.h
+.\rvmdk\project0.o: ..\..\..\..\driverlib/sysctl.h
+.\rvmdk\project0.o: ..\..\..\..\driverlib/gpio.h
